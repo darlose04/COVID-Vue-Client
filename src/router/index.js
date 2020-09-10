427698f045d0954ff9ff7b496b16571f2e17a144
Zach Smith <zsmith126028@gmail.com>
2020-09-10 12:49:03 -0500

    import GlobalData component

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import USData from "../components/data/usData/USData";
+import GlobalData from "../components/data/globalData/GlobalData";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "USData",
     component: USData,
+  },
+  {
+    path: "/global",
+    name: "GlobalData",
+    component: GlobalData,
   },
   {
     path: "/about",
