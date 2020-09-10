389feed556ccd9b7c0e4ebbfe25432e23cb46111
Zach Smith <zsmith126028@gmail.com>
2020-09-10 10:36:05 -0500

    add header component. move routes from app to header

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="header">
+    <div id="nav">
+      <h1 class="logo">
+        <i class="fas fa-viruses"></i>
+      </h1>
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+</style>
