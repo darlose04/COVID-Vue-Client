f34648d302233658d15a16b0a17f48dbec35bbcd
Zach Smith <zsmith126028@gmail.com>
2020-09-10 13:26:07 -0500

    import components, add styling

--- src/components/data/usData/USData.vue
+++ src/components/data/usData/USData.vue
@@ -1,13 +1,35 @@
 <template>
-  <div>
+  <div class="data-wrapper">
+    <Disclaimer />
     <h1>This is the page for displaying US Data</h1>
+    <StateList />
   </div>
 </template>
 
 <script>
+import Disclaimer from "../../layout/Disclaimer";
+import StateList from "./StateList";
+
 export default {
   name: "USData",
+  components: {
+    Disclaimer,
+    StateList,
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.data-wrapper {
+  width: 1400px;
+  margin: auto;
+}
+
+.data {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 1rem;
+  height: 2000px;
+  margin-bottom: 4rem;
+}
+</style>
