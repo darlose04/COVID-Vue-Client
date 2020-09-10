e24766d1221b4677b3c8b1fe4417ec3c128ebf75
Zach Smith <zsmith126028@gmail.com>
2020-09-10 13:27:20 -0500

    add styles

--- src/components/data/usData/StateList.vue
+++ src/components/data/usData/StateList.vue
@@ -27,4 +27,31 @@ export default {
   padding: 1rem 1rem;
   max-height: 1000px;
 }
+
+.state-list {
+  height: 1823px;
+  overflow: scroll;
+}
+
+.state-list h2 {
+  border-bottom: 2px solid black;
+  margin-bottom: 0.5rem;
+}
+
+.state-list ul li {
+  border-bottom: 1px dotted black;
+  margin: 0.5rem 0;
+}
+
+.state-list ul li:last-child {
+  border-bottom: none;
+}
+
+.state-list ul li h3 {
+  cursor: pointer;
+}
+
+.state-list ul li h3:hover {
+  color: red;
+}
 </style>
