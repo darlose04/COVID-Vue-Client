29343393eef9db51ddeb1e4fa25825b399fcfda7
Zach Smith <zsmith126028@gmail.com>
2020-09-10 12:36:38 -0500

    create Spinner component

---
+++ src/components/layout/Spinner.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="spinner">
+    <img v-bind:spinner="spinner" alt="Loading Gif" />
+  </div>
+</template>
+
+<script>
+import spinner from "../../assets/Spin-1s-200px.gif";
+
+export default {
+  name: "Spinner",
+};
+</script>
