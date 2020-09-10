d2382c7360c1a2c70cd0e969b4bcd7dad8c03634
Zach Smith <zsmith126028@gmail.com>
2020-09-10 12:47:56 -0500

    create component that will be for global data

---
+++ src/components/data/globalData/GlobalData.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>This is the page for displaying the global data</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GlobalData",
+};
+</script>
+
+<style scoped></style>
