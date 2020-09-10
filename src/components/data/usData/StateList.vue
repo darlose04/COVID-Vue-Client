ec6aa7f53747affc5169131146831abebb43d680
Zach Smith <zsmith126028@gmail.com>
2020-09-10 13:26:23 -0500

    add styling

--- src/components/data/usData/StateList.vue
+++ src/components/data/usData/StateList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="list-data">
-    <!-- Totals component goes here -->
+    <Totals />
     <div class="state-list">
       <h2>States / Territories</h2>
       <ul>
@@ -11,9 +11,20 @@
 </template>
 
 <script>
+import Totals from "./Totals";
+
 export default {
   name: "StateList",
+  components: {
+    Totals,
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.list-data {
+  margin-left: 1rem;
+  padding: 1rem 1rem;
+  max-height: 1000px;
+}
+</style>
