ddbfa68c63143357733f63127e23d416e75ef0f2
Zach Smith <zsmith126028@gmail.com>
2020-09-10 13:26:29 -0500

    add styling

--- src/components/data/usData/Totals.vue
+++ src/components/data/usData/Totals.vue
@@ -11,3 +11,27 @@
     </ul>
   </div>
 </template>
+
+<script>
+export default {
+  name: "Totals",
+};
+</script>
+
+<style scoped>
+.totals {
+  margin-bottom: 1rem;
+  border-bottom: 1px dotted black;
+  height: 170px;
+}
+
+.totals h2 {
+  border-bottom: 2px solid black;
+  margin-bottom: 0.5rem;
+}
+
+.US-Totals:hover {
+  cursor: pointer;
+  color: red;
+}
+</style>
