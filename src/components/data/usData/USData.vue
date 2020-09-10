d9ce8c1845ffb705ed5e309d69fcc7ef6d1e09f3
Zach Smith <zsmith126028@gmail.com>
2020-09-10 12:48:04 -0500

    create component for us data

---
+++ src/components/data/usData/USData.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>This is the page for displaying US Data</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "USData",
+};
+</script>
+
+<style scoped></style>
