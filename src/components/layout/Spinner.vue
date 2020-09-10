52d99891c053dd450f3357baad06f174f437e4b4
Zach Smith <zsmith126028@gmail.com>
2020-09-10 12:40:43 -0500

    change src for spinner gif

--- src/components/layout/Spinner.vue
+++ src/components/layout/Spinner.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="spinner">
-    <img v-bind:spinner="spinner" alt="Loading Gif" />
+    <img src="../../assets/Spin-1s-200px.gif" alt="Loading Gif" />
   </div>
 </template>
 
 <script>
-import spinner from "../../assets/Spin-1s-200px.gif";
-
 export default {
   name: "Spinner",
 };
