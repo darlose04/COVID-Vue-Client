f2f9985aae80ac3a7f648114b1299c4fab4b5127
Zach Smith <zsmith126028@gmail.com>
2020-09-10 12:59:27 -0500

    create initial Totals component

---
+++ src/components/data/usData/Totals.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="totals">
+    <h2 class="US-Totals">U.S. Totals</h2>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
+</template>
