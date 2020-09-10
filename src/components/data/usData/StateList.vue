b0f79e01b8a83d0dee33536ff931c78a09eee340
Zach Smith <zsmith126028@gmail.com>
2020-09-10 12:58:17 -0500

    create initial component for the state list

---
+++ src/components/data/usData/StateList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="list-data">
+    <!-- Totals component goes here -->
+    <div class="state-list">
+      <h2>States / Territories</h2>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StateList",
+};
+</script>
+
+<style scoped></style>
