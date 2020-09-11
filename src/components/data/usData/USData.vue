e0dfbd8525608b1fb503dc00707a947710fd25d1
Zach Smith <zsmith126028@gmail.com>
2020-09-11 11:33:27 -0500

    trying to create methods to fetch data before the StateList component is rendered. Still having trouble with Vue just not fetching data or passing it down as props

--- src/components/data/usData/USData.vue
+++ src/components/data/usData/USData.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="data-wrapper">
     <Disclaimer />
-    <StateList v-bind:dailyReport="dailyReport" />
+    <div v-if="loading">
+      <Spinner />
+    </div>
+    <div v-else>
+      <StateList v-bind:dailyReport="dailyReport" />
+    </div>
   </div>
 </template>
 
 <script>
 import Disclaimer from "../../layout/Disclaimer";
+import Spinner from "../../layout/Spinner";
 import StateList from "./StateList";
 
 import axios from "axios";
@@ -17,6 +23,7 @@ export default {
   name: "USData",
   components: {
     Disclaimer,
+    Spinner,
     StateList,
   },
   data() {
@@ -28,19 +35,35 @@ export default {
       loading: true,
     };
   },
-  beforeCreate() {
-    axios
-      .get(`${baseUrl}/coronacases`)
-      .then((res) => (this.cases = res.data))
-      .catch((err) => console.log(err));
-    axios
-      .get(`${baseUrl}/coronadeaths`)
-      .then((res) => (this.deaths = res.data))
-      .catch((err) => console.log(err));
-    axios
-      .get(`${baseUrl}/dailyreport`)
-      .then((res) => (this.dailyReport = res.data))
-      .catch((err) => console.log(err));
+  methods: {
+    // checkData() {
+    //   if (
+    //     this.cases.length > 0 &&
+    //     this.deaths.length > 0 &&
+    //     this.dailyReport.length > 0
+    //   ) {
+    //     this.loading = false;
+    //   }
+    // },
+    fetchData() {
+      axios
+        .get(`${baseUrl}/coronacases`)
+        .then((res) => (this.cases = res.data))
+        .catch((err) => console.log(err));
+      axios
+        .get(`${baseUrl}/coronadeaths`)
+        .then((res) => (this.deaths = res.data))
+        .catch((err) => console.log(err));
+      axios
+        .get(`${baseUrl}/dailyreport`)
+        .then((res) => (this.dailyReport = res.data))
+        .catch((err) => console.log(err));
+      this.loading = false;
+    },
+  },
+  created() {
+    this.fetchData();
+    // this.checkData();
   },
 };
 </script>
