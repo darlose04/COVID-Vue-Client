73a9a57333a901d095b48bd9d33badf702b42e24
Zach Smith <zsmith126028@gmail.com>
2020-09-12 13:08:05 -0500

    add style and formatting for county list

--- src/components/data/usData/CountyList.vue
+++ src/components/data/usData/CountyList.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <h1>This is where the county list will go</h1>
+  <div class="county-list">
+    <div>
+      <h1>This is where the county list will go</h1>
+    </div>
   </div>
 </template>
 
@@ -10,4 +12,24 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.county-list {
+  margin-right: 1rem;
+  padding: 1rem;
+  height: 2035px;
+  overflow: scroll;
+}
+
+.county-list div h1 {
+  border-bottom: 2px solid black;
+}
+
+.county-list div ul li {
+  margin: 0.5rem 0;
+  border-bottom: 1px dotted black;
+}
+
+.county-list div ul li:last-child {
+  border: none;
+}
+</style>
