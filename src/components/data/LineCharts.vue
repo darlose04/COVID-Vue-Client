8102021ae81099a196ce74a728b6d2a15a56c53f
Zach Smith <zsmith126028@gmail.com>
2020-09-14 12:41:12 -0500

    create method to get data to display in chart

--- src/components/data/LineCharts.vue
+++ src/components/data/LineCharts.vue
@@ -14,6 +14,7 @@ export default {
   data() {
     return {
       chartLabel: [],
+      infoArray: [],
     };
   },
   methods: {
@@ -22,7 +23,15 @@ export default {
       dateArray.push(Object.keys(this.info[0]));
       this.chartLabel = dateArray[0].slice(7, dateArray[0].length);
     },
-    getChartData() {},
+    getChartData() {
+      this.chartLabel.map((date) => {
+        let numInfo = 0;
+        this.info.map((item) => {
+          numInfo += item[`${date}`];
+        });
+        this.infoArray.push(numInfo);
+      });
+    },
   },
   created() {
     this.getChartLabels();
