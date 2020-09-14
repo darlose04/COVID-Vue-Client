1ddde5d9a02e17059b045675546cab587a1c194f
Zach Smith <zsmith126028@gmail.com>
2020-09-14 12:17:19 -0500

    create method to get chart labels

--- src/components/data/LineCharts.vue
+++ src/components/data/LineCharts.vue
@@ -1,12 +1,34 @@
 <template>
-  <div>
+  <div class="line-chart">
     <h1>This is where the charts will go</h1>
   </div>
 </template>
 
 <script>
+import { Line } from "vue-chartjs";
+
 export default {
   name: "LineCharts",
+  extends: Line,
+  props: ["info"],
+  data() {
+    return {
+      chartLabel: [],
+    };
+  },
+  methods: {
+    getChartLabels() {
+      const dateArray = [];
+      dateArray.push(Object.keys(this.info[0]));
+      this.chartLabel = dateArray[0].slice(7, dateArray[0].length);
+    },
+    getChartData() {},
+  },
+  created() {
+    this.getChartLabels();
+    this.getChartData();
+  },
+  mounted() {},
 };
 </script>
 
