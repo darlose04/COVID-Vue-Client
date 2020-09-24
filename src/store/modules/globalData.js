2a7aef5038f8acc3e0b9404b4bd0e26185a8008a
Zach Smith <zsmith126028@gmail.com>
2020-09-24 12:21:15 -0500

    add deaths to state

--- src/store/modules/globalData.js
+++ src/store/modules/globalData.js
@@ -2,10 +2,12 @@ import axios from "axios";
 
 const state = {
   cases: [],
+  deaths: [],
 };
 
 const getters = {
   allCases: (state) => state.cases,
+  allDeaths: (state) => state.deaths,
 };
 
 const actions = {
@@ -15,10 +17,17 @@ const actions = {
     );
     commit("setCases", response.data);
   },
+  async fetchGlobalDeaths({ commit }) {
+    const response = await axios.get(
+      "https://www.cov-api.com/api/global/coronadeaths/"
+    );
+    commit("setDeaths", response.data);
+  },
 };
 
 const mutations = {
   setCases: (state, cases) => (state.cases = cases),
+  setDeaths: (state, deaths) => (state.deaths = deaths),
 };
 
 export default {
