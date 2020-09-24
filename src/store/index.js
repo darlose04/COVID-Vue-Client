c288f87f27bb92298a8a3955099516af496c0be5
Zach Smith <zsmith126028@gmail.com>
2020-09-24 12:26:12 -0500

    change import path of deaths

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import Vuex from "vuex";
 import Vue from "vue";
 import cases from "./modules/globalData";
-import deaths from "./modules/globalData";
+import deaths from "./modules/globalDeaths";
 
 Vue.use(Vuex);
 
