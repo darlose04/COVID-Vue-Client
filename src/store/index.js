b0d3245a1df5ceee9a27d96e26b671c8c5cab732
Zach Smith <zsmith126028@gmail.com>
2020-09-24 12:22:38 -0500

    imported deaths, but it does not work. May have to do a separate js file for deaths state

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vuex from "vuex";
 import Vue from "vue";
 import cases from "./modules/globalData";
+import deaths from "./modules/globalData";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     cases,
+    deaths,
   },
 });
