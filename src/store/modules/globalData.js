a9152347e2fd294ec747e99e164de2107965168b
Zach Smith <zsmith126028@gmail.com>
2020-09-24 12:24:00 -0500

    remove deaths related code

--- src/store/modules/globalData.js
+++ src/store/modules/globalData.js
@@ -7,7 +7,6 @@ const state = {
 
 const getters = {
   allCases: (state) => state.cases,
-  allDeaths: (state) => state.deaths,
 };
 
 const actions = {
@@ -17,17 +16,10 @@ const actions = {
     );
     commit("setCases", response.data);
   },
-  async fetchGlobalDeaths({ commit }) {
-    const response = await axios.get(
-      "https://www.cov-api.com/api/global/coronadeaths/"
-    );
-    commit("setDeaths", response.data);
-  },
 };
 
 const mutations = {
   setCases: (state, cases) => (state.cases = cases),
-  setDeaths: (state, deaths) => (state.deaths = deaths),
 };
 
 export default {
