08b528f75959f1991137460a83e93f056e101d39
Zach Smith <zsmith126028@gmail.com>
2020-09-25 12:17:41 -0500

    create mutation and action to get list of countries

--- src/store/modules/globalDailyReport.js
+++ src/store/modules/globalDailyReport.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const state = {
   dailyReport: [],
-  countries: [],
+  countryTotals: [],
 };
 
 const getters = {
@@ -69,10 +69,68 @@ const actions = {
 
     commit("setDailyReport", response.data);
   },
+
+  getCountryTotals({ commit }) {
+    let data = state.dailyReport;
+    let countryArr = [];
+
+    for (let i = 0; i < data.length; i++) {
+      if (!countryArr.includes(data[i].Country_Region)) {
+        countryArr.push(data[i].Country_Region);
+      }
+    }
+
+    let countryStats = [];
+
+    countryArr.map((country) => {
+      let singleCountryStats = [];
+      data.map((obj) => {
+        if (obj.Country_Region === country) {
+          singleCountryStats.push(obj);
+        }
+      });
+
+      countryStats.push(singleCountryStats);
+    });
+
+    let countryObjArr = [];
+    let indexCounter = 0;
+    countryStats.map((item) => {
+      let countryTotalsObj = {
+        Country_Region: item[0].Country_Region,
+        Confirmed: 0,
+        Deaths: 0,
+        Recovered: 0,
+        Index: indexCounter++,
+      };
+
+      item.map(
+        (itemObj) => (
+          (countryTotalsObj.Confirmed += itemObj.Confirmed),
+          (countryTotalsObj.Deaths += itemObj.Deaths),
+          (countryTotalsObj.Recovered += itemObj.Recovered)
+        )
+      );
+
+      countryObjArr.push(countryTotalsObj);
+    });
+
+    countryObjArr.sort((a, b) => {
+      return a.Country_Region < b.Country_region
+        ? -1
+        : a.Country_Region > b.Country_Region
+        ? 1
+        : 0;
+    });
+
+    commit("setCountryTotals", countryObjArr);
+  },
 };
 
 const mutations = {
   setDailyReport: (state, dailyReport) => (state.dailyReport = dailyReport),
+  setCountryTotals: (state, countryTotals) =>
+    (state.countryTotals = countryTotals),
 };
 
 export default {
