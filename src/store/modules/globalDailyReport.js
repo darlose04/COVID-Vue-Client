d041bc451c203840b800592b271dc03334a6d2f5
Zach Smith <zsmith126028@gmail.com>
2020-09-25 11:59:19 -0500

    map through dailyReport data and create list with countries' total data

--- src/store/modules/globalDailyReport.js
+++ src/store/modules/globalDailyReport.js
@@ -13,7 +13,60 @@ const actions = {
     const response = await axios.get(
       "https://www.cov-api.com/api/global/dailyreport/"
     );
-    commit("setDailyReport", response.data);
+
+    let data = response.data;
+    let countryArr = [];
+
+    for (let i = 0; i < data.length; i++) {
+      if (!countryArr.includes(data[i].Country_Region)) {
+        countryArr.push(data[i].Country_Region);
+      }
+    }
+
+    let countryStats = [];
+
+    countryArr.map((country) => {
+      let singleCountryStats = [];
+      data.map((obj) => {
+        if (obj.Country_Region === country) {
+          singleCountryStats.push(obj);
+        }
+      });
+
+      countryStats.push(singleCountryStats);
+    });
+
+    let countryObjArr = [];
+    let indexCounter = 0;
+    countryStats.map((item) => {
+      let countryTotalsObj = {
+        Country_Region: item[0].Country_Region,
+        Confirmed: 0,
+        Deaths: 0,
+        Recovered: 0,
+        Index: indexCounter++,
+      };
+
+      item.map(
+        (itemObj) => (
+          (countryTotalsObj.Confirmed += itemObj.Confirmed),
+          (countryTotalsObj.Deaths += itemObj.Deaths),
+          (countryTotalsObj.Recovered += itemObj.Recovered)
+        )
+      );
+
+      countryObjArr.push(countryTotalsObj);
+    });
+
+    countryObjArr.sort((a, b) => {
+      return a.Country_Region < b.Country_region
+        ? -1
+        : a.Country_Region > b.Country_Region
+        ? 1
+        : 0;
+    });
+
+    commit("setDailyReport", countryObjArr);
   },
 };
 
