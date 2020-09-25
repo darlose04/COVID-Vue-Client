0dcfe43df1e1313e2d1b5db34c95f666e5acbfae
Zach Smith <zsmith126028@gmail.com>
2020-09-25 12:05:01 -0500

    comment code in actions, going to try and do it separately in a mutation

--- src/store/modules/globalDailyReport.js
+++ src/store/modules/globalDailyReport.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const state = {
   dailyReport: [],
+  countries: [],
 };
 
 const getters = {
@@ -14,59 +15,59 @@ const actions = {
       "https://www.cov-api.com/api/global/dailyreport/"
     );
 
-    let data = response.data;
-    let countryArr = [];
+    // let data = response.data;
+    // let countryArr = [];
 
-    for (let i = 0; i < data.length; i++) {
-      if (!countryArr.includes(data[i].Country_Region)) {
-        countryArr.push(data[i].Country_Region);
-      }
-    }
+    // for (let i = 0; i < data.length; i++) {
+    //   if (!countryArr.includes(data[i].Country_Region)) {
+    //     countryArr.push(data[i].Country_Region);
+    //   }
+    // }
 
-    let countryStats = [];
+    // let countryStats = [];
 
-    countryArr.map((country) => {
-      let singleCountryStats = [];
-      data.map((obj) => {
-        if (obj.Country_Region === country) {
-          singleCountryStats.push(obj);
-        }
-      });
+    // countryArr.map((country) => {
+    //   let singleCountryStats = [];
+    //   data.map((obj) => {
+    //     if (obj.Country_Region === country) {
+    //       singleCountryStats.push(obj);
+    //     }
+    //   });
 
-      countryStats.push(singleCountryStats);
-    });
+    //   countryStats.push(singleCountryStats);
+    // });
 
-    let countryObjArr = [];
-    let indexCounter = 0;
-    countryStats.map((item) => {
-      let countryTotalsObj = {
-        Country_Region: item[0].Country_Region,
-        Confirmed: 0,
-        Deaths: 0,
-        Recovered: 0,
-        Index: indexCounter++,
-      };
+    // let countryObjArr = [];
+    // let indexCounter = 0;
+    // countryStats.map((item) => {
+    //   let countryTotalsObj = {
+    //     Country_Region: item[0].Country_Region,
+    //     Confirmed: 0,
+    //     Deaths: 0,
+    //     Recovered: 0,
+    //     Index: indexCounter++,
+    //   };
 
-      item.map(
-        (itemObj) => (
-          (countryTotalsObj.Confirmed += itemObj.Confirmed),
-          (countryTotalsObj.Deaths += itemObj.Deaths),
-          (countryTotalsObj.Recovered += itemObj.Recovered)
-        )
-      );
+    //   item.map(
+    //     (itemObj) => (
+    //       (countryTotalsObj.Confirmed += itemObj.Confirmed),
+    //       (countryTotalsObj.Deaths += itemObj.Deaths),
+    //       (countryTotalsObj.Recovered += itemObj.Recovered)
+    //     )
+    //   );
 
-      countryObjArr.push(countryTotalsObj);
-    });
+    //   countryObjArr.push(countryTotalsObj);
+    // });
 
-    countryObjArr.sort((a, b) => {
-      return a.Country_Region < b.Country_region
-        ? -1
-        : a.Country_Region > b.Country_Region
-        ? 1
-        : 0;
-    });
+    // countryObjArr.sort((a, b) => {
+    //   return a.Country_Region < b.Country_region
+    //     ? -1
+    //     : a.Country_Region > b.Country_Region
+    //     ? 1
+    //     : 0;
+    // });
 
-    commit("setDailyReport", countryObjArr);
+    commit("setDailyReport", response.data);
   },
 };
 
