d0b6e38729dc4c6d2387f63f485350878bba677d
Zach Smith <zsmith126028@gmail.com>
2020-09-29 09:35:53 -0500

    import Disclaimer and CountryList

--- src/components/data/globalData/GlobalData.vue
+++ src/components/data/globalData/GlobalData.vue
@@ -1,14 +1,26 @@
 <template>
   <div>
-    <h1>This is the page for displaying the global data</h1>
+    <Disclaimer />
+    <div>
+      <!-- <Spinner /> -->
+      <CountryList />
+    </div>
   </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from "vuex";
+import Disclaimer from "../../layout/Disclaimer";
+// import Spinner from "../../layout/Spinner";
+import CountryList from "./CountryList";
 
 export default {
   name: "GlobalData",
+  components: {
+    Disclaimer,
+    // Spinner,
+    CountryList,
+  },
   methods: {
     ...mapActions([
       "fetchGlobalCases",
