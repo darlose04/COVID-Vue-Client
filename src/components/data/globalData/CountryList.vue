23c50a65712c9af27f465647d82178f8fd9a74b6
Zach Smith <zsmith126028@gmail.com>
2020-09-29 08:53:28 -0500

    creating CountryList component

---
+++ src/components/data/globalData/CountryList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="list-data">
+    <GlobalTotals />
+  </div>
+</template>
+
+<script>
+import GlobalTotals from "./GlobalTotals";
+
+export default {
+  name: "CountryList",
+  components: {
+    GlobalTotals,
+  },
+};
+</script>
+
+<style scoped>
+.country-list {
+  height: 1863px;
+  overflow: scroll;
+}
+
+.country-list h2 {
+  border-bottom: 2px solid black;
+  margin-bottom: 0.5rem;
+}
+
+.country-list ul li {
+  border-bottom: 1px dotted black;
+  margin: 0.5rem 0;
+}
+
+.country-list ul li:last-child {
+  border-bottom: none;
+}
+
+.country-list ul li h3 {
+  cursor: pointer;
+}
+
+.country-list ul li h3:hover {
+  color: red;
+}
+</style>
