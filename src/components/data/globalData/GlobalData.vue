6299aec0b1a937f360a808021befd680c96d8b0b
Zach Smith <zsmith126028@gmail.com>
2020-09-29 11:44:31 -0500

    import Spinner component properly

--- src/components/data/globalData/GlobalData.vue
+++ src/components/data/globalData/GlobalData.vue
@@ -13,14 +13,14 @@
 <script>
 import { mapGetters, mapActions } from "vuex";
 import Disclaimer from "../../layout/Disclaimer";
-// import Spinner from "../../layout/Spinner";
+import Spinner from "../../layout/Spinner";
 import CountryList from "./CountryList";
 
 export default {
   name: "GlobalData",
   components: {
     Disclaimer,
-    // Spinner,
+    Spinner,
     CountryList,
   },
   methods: {
@@ -45,7 +45,6 @@ export default {
   },
   mounted() {
     // console.log("GlobalData Component: ");
-    console.log(this.getDailyReport);
   },
 };
 </script>
