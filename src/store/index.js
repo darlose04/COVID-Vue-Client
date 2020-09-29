d70f357abeed1408a159b0a9398ccaf9ea3bda37
Zach Smith <zsmith126028@gmail.com>
2020-09-29 10:03:38 -0500

    change name of import from globalDailyReport module

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,7 @@ import Vuex from "vuex";
 import Vue from "vue";
 import cases from "./modules/globalCases";
 import deaths from "./modules/globalDeaths";
-// import dailyReport from "./modules/globalDailyReport";
-import countryTotals from "./modules/globalDailyReport";
+import globalDailyReport from "./modules/globalDailyReport";
 
 Vue.use(Vuex);
 
@@ -11,7 +10,6 @@ export default new Vuex.Store({
   modules: {
     cases,
     deaths,
-    // dailyReport,
-    countryTotals,
+    globalDailyReport,
   },
 });
