0984c09749c70e9dc138ee0468d4783ced9e6b3c
Zach Smith <zsmith126028@gmail.com>
2020-10-05 12:54:03 -0500

    create action to get us totals, but I can't use the dailyReport info to do it, so I'll need to move this to another module

--- src/store/modules/usDailyReport.js
+++ src/store/modules/usDailyReport.js
@@ -18,6 +18,29 @@ const actions = {
     );
     commit("setUSDailyReport", response.data);
   },
+  createUSTotals({ commit, state }) {
+    let usData = [...state.usDailyReport];
+
+    let usTotals = {
+      totalCases: 0,
+      totalDeaths: 0,
+      totalActive: 0,
+      totalHospitalized: 0,
+      totalTested: 0,
+      totalRecovered: 0,
+    };
+
+    usData.map(
+      (state) => (usTotals.totalCases += state.Confirmed),
+      (usTotals.totalDeaths += state.Deaths),
+      (usTotals.totalActive += state.Active),
+      (usTotals.totalHospitalized += state.People_Hospitalized),
+      (usTotals.totalTested += state.People_Tested),
+      (usTotals.totalRecovered += state.Recovered)
+    );
+
+    commit("setUSTotals", usTotals);
+  },
 };
 
 const mutations = {
