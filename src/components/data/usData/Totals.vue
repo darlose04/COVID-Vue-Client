5fae81310e0e61e4081062163518a5321217ed48
Zach Smith <zsmith126028@gmail.com>
2020-10-05 13:16:00 -0500

    remove commented code not needed. re-add numWithCommas since the reason it was undefined was because the vuex mutation was not completed

--- src/components/data/usData/Totals.vue
+++ src/components/data/usData/Totals.vue
@@ -4,12 +4,14 @@
       U.S. Totals
     </h2>
     <ul>
-      <li>Confirmed: {{ this.usTotals.totalCases }}</li>
-      <li>Deaths: {{ this.usTotals.totalDeaths }}</li>
-      <li>Active: {{ this.usTotals.totalActive }}</li>
-      <li>Hospitalized: {{ this.usTotals.totalHospitalized }}</li>
-      <li>Tested: {{ this.usTotals.totalTested }}</li>
-      <li>Recovered: {{ this.usTotals.totalRecovered }}</li>
+      <li>Confirmed: {{ numWithCommas(this.usTotals.totalCases) }}</li>
+      <li>Deaths: {{ numWithCommas(this.usTotals.totalDeaths) }}</li>
+      <li>Active: {{ numWithCommas(this.usTotals.totalActive) }}</li>
+      <li>
+        Hospitalized: {{ numWithCommas(this.usTotals.totalHospitalized) }}
+      </li>
+      <li>Tested: {{ numWithCommas(this.usTotals.totalTested) }}</li>
+      <li>Recovered: {{ numWithCommas(this.usTotals.totalRecovered) }}</li>
     </ul>
   </div>
 </template>
@@ -22,41 +24,14 @@ export default {
   props: ["changeStateName"],
   methods: {
     ...mapActions(["createUSTotals"]),
-    // numWithCommas(num) {
-    //   return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    // },
+    numWithCommas(num) {
+      return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    },
   },
   computed: {
     ...mapGetters({
       usTotals: "getUSTotals",
     }),
-    // getTotalData: function() {
-    //   let totalCases = 0;
-    //   let totalDeaths = 0;
-    //   let totalActive = 0;
-    //   let totalHospitalized = 0;
-    //   let totalTested = 0;
-    //   let totalRecovered = 0;
-    //   this.dailyReport.map((state) => {
-    //     totalCases += state.Confirmed;
-    //     totalDeaths += state.Deaths;
-    //     totalActive += state.Active;
-    //     totalHospitalized += state.People_Hospitalized;
-    //     totalTested += state.People_Tested;
-    //     totalRecovered += state.Recovered;
-    //   });
-    //   return {
-    //     totalCases,
-    //     totalDeaths,
-    //     totalActive,
-    //     totalHospitalized,
-    //     totalTested,
-    //     totalRecovered,
-    //   };
-    // },
-  },
-  created() {
-    this.createUSTotals();
   },
 };
 </script>
