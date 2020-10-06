07447f7c96f2738cf948536f888b9b26b0a1f94d
Zach Smith <zsmith126028@gmail.com>
2020-10-06 15:15:38 -0500

    add loop through gettter to display county info for selected state

--- src/components/data/usData/CountyList.vue
+++ src/components/data/usData/CountyList.vue
@@ -2,11 +2,11 @@
   <div class="county-list">
     <div>
       <h1>{{ this.stateName }}</h1>
-      <ul v-for="county in this.countyCaseTotals" v-bind:key="county.id">
+      <ul v-for="county in this.countyObjects" v-bind:key="county.id">
         <li>
           <h3>{{ county.county }}</h3>
           <p>Cases: {{ county.cases }}</p>
-          <!-- <p>Deaths: {{ county.deaths }}</p> -->
+          <p>Deaths: {{ county.deaths }}</p>
         </li>
       </ul>
     </div>
@@ -51,7 +51,7 @@ export default {
     ...mapGetters({
       countyCases: "getUSCountyCases",
       countyDeaths: "getUSCountyDeaths",
-      countyCasesObjects: "getUSStateCountyObjects",
+      countyObjects: "getUSStateCountyObjects",
       // countyCaseTotals: "getUSStateCountyTotals",
     }),
   },
