547506e35af779f0dbb89fff51a04b018435ce43
Zach Smith <zsmith126028@gmail.com>
2020-10-06 10:55:27 -0500

    create module for getting us deaths

---
+++ src/store/modules/usDeaths.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+const state = {
+  usDeaths: [],
+  usCountyDeaths: [],
+};
+
+const getters = {
+  getUSDeaths: (state) => state.usDeaths,
+  getUSCountyDeaths: (state) => state.usCountyDeaths,
+};
+
+const actions = {
+  async fetchUSDeaths({ commit }) {
+    const response = await axios.get(
+      "https://www.cov-api.com/api/usa/coronadeaths/"
+    );
+    commit("setUSDeaths", response.data);
+  },
+
+  createUSCountyDeaths({ commit, state }, nameOfState) {
+    let deaths = [...state.usDeaths];
+    let stateCounties = [];
+
+    deaths.map((county) => {
+      if (county.State === nameOfState) {
+        stateCounties.push(county);
+      }
+    });
+
+    commit("setCountyDeaths", stateCounties);
+  },
+};
+
+const mutations = {
+  setUSDeaths: (state, deaths) => (state.usDeaths = deaths),
+  setUSCountyDeaths: (state, deaths) => (state.usCountyDeaths = deaths),
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations,
+};
