900a94d46fb2e3bacd81798fcdf7d66feb863a6c
Zach Smith <zsmith126028@gmail.com>
2020-10-06 12:11:57 -0500

    create action to loop through and get the recent stats

--- src/store/modules/usCases.js
+++ src/store/modules/usCases.js
@@ -3,11 +3,13 @@ import axios from "axios";
 const state = {
   usCases: [],
   usCountyCases: [],
+  usStateCountyTotals: [],
 };
 
 const getters = {
   getUSCases: (state) => state.usCases,
   getUSCountyCases: (state) => state.usCountyCases,
+  getUSStateCountyTotals: (state) => state.usStateCountyTotals,
 };
 
 const actions = {
@@ -32,14 +34,31 @@ const actions = {
     commit("setUSCountyCases", stateCounties);
   },
 
-  // addUSCountyCases({ commit, state }) {
-  //   let countyCases
-  // }
+  addUSCountyCases({ commit, state }) {
+    let countyCases = [...state.usCountyCases];
+    let stateCountyTotalsObjects = [];
+    let dateArray = [];
+    dateArray.push(Object.keys(countyCases[0]));
+    let dates = dateArray[0].slice(7, dateArray[0].length);
+    let recentDate = dates[dates.length - 1];
+
+    for (let i = 0; i < countyCases.length; i++) {
+      let stateObj = {
+        id: countyCases[i].UID,
+        county: countyCases[i].City,
+        cases: countyCases[i][recentDate],
+      };
+      stateCountyTotalsObjects.push(stateObj);
+    }
+
+    commit("setUSStateCountyTotals", stateCountyTotalsObjects);
+  },
 };
 
 const mutations = {
   setUSCases: (state, cases) => (state.usCases = cases),
   setUSCountyCases: (state, cases) => (state.usCountyCases = cases),
+  setUSStateCountyTotals: (state, cases) => (state.usStateCountyTotals = cases),
 };
 
 export default {
