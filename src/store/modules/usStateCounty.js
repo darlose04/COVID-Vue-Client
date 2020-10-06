b6d5ed4d74fd8efd6b7f62d8e52075633df7a310
Zach Smith <zsmith126028@gmail.com>
2020-10-06 15:13:19 -0500

    finish action that creates objects for each state's counties

--- src/store/modules/usStateCounty.js
+++ src/store/modules/usStateCounty.js
@@ -8,10 +8,27 @@ const getters = {
 
 const actions = {
   createUSCountyObjects({ commit, rootState }) {
-    let stateCountyObjects = [];
-    console.log(rootState.usCases);
+    let countyCases = [...rootState.usCases.usCountyCases];
+    let countyDeaths = [...rootState.usDeaths.usCountyDeaths];
 
-    commit("setUSStateCountyObjects", stateCountyObjects);
+    let stateCountyTotalsObjects = [];
+    let dateArray = [];
+    dateArray.push(Object.keys(countyCases[0]));
+    let dates = dateArray[0].slice(7, dateArray[0].length);
+    let recentDate = dates[dates.length - 1];
+
+    for (let i = 0; i < countyCases.length; i++) {
+      let stateCountyObj = {
+        id: countyCases[i].UID,
+        county: countyCases[i].City,
+        cases: countyCases[i][recentDate],
+        deaths: countyDeaths[i][recentDate],
+      };
+
+      stateCountyTotalsObjects.push(stateCountyObj);
+    }
+
+    commit("setUSStateCountyObjects", stateCountyTotalsObjects);
   },
 };
 
