76eca10b04ecda9d9f60f3d7bf3fe245099cf3a3
Zach Smith <zsmith126028@gmail.com>
2020-10-07 11:04:11 -0500

    add state and getters for us line charts

---
+++ src/store/modules/usLineCharts.js
@@ -0,0 +1,22 @@
+const state = {
+  usChartLabel: [],
+  usChartCases: [],
+  usChartDeaths: [],
+};
+
+const getters = {
+  getUSChartLabel: (state) => state.usChartLabel,
+  getUSChartCases: (state) => state.usChartCases,
+  getUSChartDeaths: (state) => state.usChartDeaths,
+};
+
+const actions = {};
+
+const mutations = {};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations,
+};
