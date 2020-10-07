92bae43fa77f440076092d434b143580c4160959
Zach Smith <zsmith126028@gmail.com>
2020-10-07 15:12:32 -0500

    trying to figure out where the issue is, but I cannot seem to find why the charts are not updating with new data when the stateName changes

--- src/components/data/LineCharts.vue
+++ src/components/data/LineCharts.vue
@@ -1,25 +1,7 @@
 <template>
   <div class="line-chart">
     <div v-if="usChartLabel.length > 0">
-      <div v-if="stateName !== ''">
-        <div v-if="usStateChartCases.length > 0">
-          <ChartInfo
-            v-bind:chartLabel="usChartLabel"
-            v-bind:infoArray="usStateChartCases"
-            v-bind:color="casesColor"
-            v-bind:label="stateName + ' Cases'"
-          />
-        </div>
-        <div v-if="usStateChartDeaths.length > 0">
-          <ChartInfo
-            v-bind:chartLabel="usChartLabel"
-            v-bind:infoArray="usStateChartDeaths"
-            v-bind:color="deathsColor"
-            v-bind:label="stateName + ' Deaths'"
-          />
-        </div>
-      </div>
-      <div v-else>
+      <div v-if="stateName === ''">
         <div v-if="usChartCases.length > 0">
           <ChartInfo
             v-bind:chartLabel="usChartLabel"
@@ -34,6 +16,24 @@
             v-bind:infoArray="usChartDeaths"
             v-bind:color="deathsColor"
             v-bind:label="'Total U.S. Deaths'"
+          />
+        </div>
+      </div>
+      <div v-else>
+        <div v-if="usStateChartCases.length > 0">
+          <ChartInfo
+            v-bind:chartLabel="usChartLabel"
+            v-bind:infoArray="usStateChartCases"
+            v-bind:color="casesColor"
+            v-bind:label="stateName + ' Cases'"
+          />
+        </div>
+        <div v-if="usStateChartDeaths.length > 0">
+          <ChartInfo
+            v-bind:chartLabel="usChartLabel"
+            v-bind:infoArray="usStateChartDeaths"
+            v-bind:color="deathsColor"
+            v-bind:label="stateName + ' Deaths'"
           />
         </div>
       </div>
@@ -76,15 +76,18 @@ export default {
     }),
   },
   mounted() {
-    console.log(this.stateName);
     this.createUSChartLabel();
     this.createUSChartCases();
+    console.log("Chart cases in mounted hook");
+    console.log(this.stateName);
+    console.log(this.usChartCases);
     this.createUSChartDeaths();
   },
   beforeUpdate() {
-    console.log(this.stateName);
     this.createUSChartLabel();
     this.createUSStateChartCases(this.stateName);
+    console.log("State chart cases in beforeUpdate hook");
+    console.log(this.stateName);
     console.log(this.usStateChartCases);
     this.createUSStateChartDeaths(this.stateName);
   },
