550b1a98c33d5cd1fe9feb309cb566f0cb5ea591
Zach Smith <zsmith126028@gmail.com>
2020-10-08 10:54:52 -0500

    fix typo

--- src/components/data/globalData/GlobalLineCharts.vue
+++ src/components/data/globalData/GlobalLineCharts.vue
@@ -55,7 +55,7 @@ export default {
   },
   methods: {
     ...mapActions([
-      "createglobalChartLabel",
+      "createGlobalChartLabel",
       "createGlobalChartCases",
       "createGlobalChartDeaths",
       "createCountryChartCases",
