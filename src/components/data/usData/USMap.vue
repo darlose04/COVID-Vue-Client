e9f99ba01ed2d7dc024d08ad651e28e920342716
Zach Smith <zsmith126028@gmail.com>
2020-10-15 11:02:29 -0500

    no idea how to get this to work. documentation is a little lacking and I have never used svg maps before

--- src/components/data/usData/USMap.vue
+++ src/components/data/usData/USMap.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="data-wrapper">
     <svg-map class="map" :map="USA" :location-class="getLocationClass" @mouseover="pointLocation" @mouseout="unpointLocation" @mousemove="moveOnLocation" />
-    <div></div>
+    <div class="tooltips" :style="tootipStyle">
+      {{ pointedLocation }}
+    </div>
   </div>
 </template>
 
 <script>
 import { SvgMap } from 'vue-svg-map';
 import USA from "@svg-maps/usa";
+import getLocationName from '../../../utilities';
 
 export default {
   name: "USMap",
@@ -23,7 +26,21 @@ export default {
   },
   methods: {
     pointLocation(event) {
-      this.pointedLocation = 
+      this.pointedLocation = getLocationName(event.target)
+    },
+    unpointLocation() {
+      this.pointedLocation = null;
+      this.tooltipStyle = { display: 'none' }
+    },
+    moveOnLocation(event) {
+      this.tooltipStyle = {
+        display: "block",
+        top: `${event.clientY + 10}px`,
+        left: `${event.clientX - 100}px`,
+      }
+    },
+    getLocationClass(location, index) {
+      return `svg-map__location svg-map__location--heat${index % 4}`
     }
   }
 }
