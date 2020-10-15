368d3c3b3d96e5c3f624c539b733e6e0db964670
Zach Smith <zsmith126028@gmail.com>
2020-10-15 10:08:56 -0500

    add route for USMap component

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import USData from "../components/data/usData/USData";
 import GlobalData from "../components/data/globalData/GlobalData";
+import USMap from "../components/data/usData/USMap";
 
 Vue.use(VueRouter);
 
@@ -10,6 +11,11 @@ const routes = [
     path: "/",
     name: "USData",
     component: USData,
+  },
+  {
+    path: "/usmap",
+    name: "USMap",
+    component: USMap,
   },
   {
     path: "/global",
