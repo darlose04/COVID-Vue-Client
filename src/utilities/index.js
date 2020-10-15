84a8f1fd11a714e8bfdaff3ea293657164164683
Zach Smith <zsmith126028@gmail.com>
2020-10-15 10:46:55 -0500

    add file get location name on maps. copied from github repo which is listed in comments in file

---
+++ src/utilities/index.js
@@ -0,0 +1,27 @@
+/* 
+  Copied from https://github.com/VictorCazanave/vue-svg-map/blob/master/demo/src/utilities/index.js
+*/
+
+/**
+ * Get the name of the targeted location
+ *
+ * @param {Node} node - HTML node
+ * @returns {string} Name of the location
+ */
+export function getLocationName(node) {
+  return node && node.attributes.name.value;
+}
+
+/**
+ * Get the name of the selected location
+ *
+ * @param {Object} map - Map of component
+ * @param {string} locationId - Id of selected location
+ * @returns {string} Name of the selected location
+ */
+export function getSelectedLocationName(map, locationId) {
+  return (
+    locationId &&
+    map.locations.find((location) => location.id === locationId).name
+  );
+}
